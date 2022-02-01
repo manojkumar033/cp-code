4750fc83388e9ed9104da95cb6b91b2d49b8488f
manojkumar033 <60658898+manojkumar033@users.noreply.github.com>
2022-02-01 23:57:49 +0530

    Add files via upload

---
+++ templet.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define pb push_back
+#define floop(i, a, b) for (int i = a; i < b; i++)
+std::vector<int> arry(int n)
+{
+    std::vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    return v;
+}
+void Print(vi vec)
+{
+    for (int i = 0; i < vec.size(); i++)
+    {
+        cout << vec[i] << " ";
+    }
+}
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+
+void solve()
+{
+}
+
+////////////////////////////////////////////////////////////
+main()
+{
+
+    // int t;
+    // cin >> t;
+    // while (t--)
+    // {
+    //     solve();
+    // }
+    solve();
+    return 0;
+}
